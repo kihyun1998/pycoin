9c3cb661ca26016c1a12233b6c8e836d2ed816b4
박기현 <kihyun1998@naver.com>
2024-03-27 20:34:08 +0900

    Create chiffrer.py

---
+++ chiffrer.py
@@ -0,0 +1,36 @@
+
+import json
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+
+filename = 'key/key.json'
+
+# Charger les donnees
+with open(filename, 'r') as file:
+    donnees = json.load(file)
+
+donnees_bytes = json.dumps(donnees).encode('utf-8')
+
+
+# Charger la cle publique
+with open("cles/cle_publique.pem","rb") as f:
+    cle_publique = serialization.load_pem_public_key(
+        f.read(),
+        backend=default_backend()
+    )
+
+# Chiffrer le donnees
+chiffre = cle_publique.encrypt(
+    donnees_bytes,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+# Enregistrez les donnes cryptes
+with open("cles/chiffrer_donnes.bin","wb") as f:
+    f.write(chiffre)
