9a3b5ab8e432216d5284186704917ab6a0c27f7f
박기현 <kihyun1998@naver.com>
2024-03-28 20:06:23 +0900

    로그인 구현

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import ccxt
+
 from securite.decrypter import decrypter_les_donnees
 
 data = decrypter_les_donnees()
 cle_API = data["api_key"]
 clef_screte = data["secret_key"]
+
+
+exchange = ccxt.binance(config={
+    'apiKey':cle_API,
+    'secret':clef_screte,
+    'enableRateLimit':True,
+    'options':{
+        'defaultType':'future'
+    }
+})
+
+print(exchange)
