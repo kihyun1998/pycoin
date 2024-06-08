c013b505cb78135f9cf142f1860406c4fdbcc91d
박기현 <kihyun1998@naver.com>
2024-06-08 21:18:41 +0900

    하이켄아시 개선

--- algorithm/heikin_ashi.py
+++ algorithm/heikin_ashi.py
@@ -1,15 +1,14 @@
 from math_extention.math_extention import perfectRound
 
+
 def heikin_ashi(df):
     ha_df = df[['Open','High','Low','Close']].copy()
     ha_df['Close'] = perfectRound(((df['Open']+df['High']+df['Low']+df['Close'])/4),2)
 
     # Heikin Ashi Open price
-    for i in range(len(df)):
-        if i==0:
-            ha_df.iat[0,0] = perfectRound(((df['Open'].iloc[0]+df['Close'].iloc[0])/2),2)
-        else:
-            ha_df.iat[i,0] = perfectRound(((ha_df.iat[i-1,0] + ha_df.iat[i-1,3])/2),2)
+    ha_df.iat[0, 0] = perfectRound((df['Open'].iloc[0] + df['Close'].iloc[0]) / 2, 2)
+    ha_df['Open'] = ha_df['Open'].shift().fillna(ha_df['Open'].iloc[0])
+    ha_df['Open'] = perfectRound((ha_df['Open'] + ha_df['Close'].shift().fillna(ha_df['Close'].iloc[0])) / 2, 2)
 
     # Heikin Ashi High price
     ha_df['High'] = ha_df.loc[:,['Open','Close']].join(df['High']).max(axis=1)
